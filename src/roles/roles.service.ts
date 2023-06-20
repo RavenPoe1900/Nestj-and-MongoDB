6cae3c3686bab4d0725e0fb31a9c437a9cb5b566
RavenPoe1900 <ravenpoe1900@gmail.com>
2023-06-20 17:29:51 -0400

    Organizing the code

--- src/roles/roles.service.ts
+++ src/roles/roles.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model, ProjectionType, QueryOptions } from 'mongoose';
-import { GenericService } from 'src/common/generic.service';
+import { Model} from 'mongoose';
+import { GenericService } from 'src/common/service/generic.service';
 import { DateRegisterModel } from 'src/database/mongoose/dateRegister/dateRegister.model';
 import { SoftDeleteModel } from 'src/database/mongoose/softDelete/softDelete.model';
 import { RoleDto } from './dto/create-role.dto';
@@ -14,10 +14,10 @@ import { Role, RoleDocument } from './schemas/role.entity';
 export class RolesService extends GenericService<Role, RoleDto, UpdateRoleDto, Model<RoleDocument>,
                                   DateRegisterModel<RoleDocument>, SoftDeleteModel<RoleDocument> > {
   constructor(
-        @InjectModel(Role.name) private userModel: Model<RoleDocument>,
+        @InjectModel(Role.name) private model: Model<RoleDocument>,
         @InjectModel(Role.name) dateRegisterModel: DateRegisterModel<RoleDocument>,
         @InjectModel(Role.name) softDeleteModel: SoftDeleteModel<RoleDocument>
         ) {
-    super(userModel, dateRegisterModel, softDeleteModel);
+    super(model, dateRegisterModel, softDeleteModel);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { FilterQuery, Model, ObjectId, ProjectionType, QueryOptions, UpdateQuery } from 'mongoose';
-import { GenericService } from 'src/common/generic.service';
+import { Model } from 'mongoose';
+import { GenericService } from 'src/common/service/generic.service';
 import { DateRegisterModel } from 'src/database/mongoose/dateRegister/dateRegister.model';
 import { SoftDeleteModel } from 'src/database/mongoose/softDelete/softDelete.model';
 import { UserDto } from './dto/create-user.dto';
@@ -12,10 +12,10 @@ import { User, UserDocument } from './schemas/user.schema';
 export class UsersService extends GenericService<User, UserDto, UpdateUserDto, Model<UserDocument>,
                                   DateRegisterModel<UserDocument>, SoftDeleteModel<UserDocument> > {
   constructor(
-        @InjectModel(User.name) private userModel: Model<UserDocument>,
+        @InjectModel(User.name) private model: Model<UserDocument>,
         @InjectModel(User.name) dateRegisterModel: DateRegisterModel<UserDocument>,
         @InjectModel(User.name) softDeleteModel: SoftDeleteModel<UserDocument>
         ) {
-    super(userModel, dateRegisterModel, softDeleteModel);
+    super(model, dateRegisterModel, softDeleteModel);
   }
 }
